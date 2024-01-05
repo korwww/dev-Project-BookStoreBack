2c4b4a0c371e205287562b451c8384a392e5512b
LeeUijin <n2n23339@gmail.com>
2024-01-06 00:20:37 +0900

    Refactor : modularized books.js callback functions

--- routes/books.js
+++ routes/books.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../mariadb');
 const { body, param, validationResult } = require('express-validator');
+const {selectAll, selectCategory, selectSingleBook} = require('../controller/BookContorller');
 
-const jwt = require('jsonwebtoken');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -21,21 +21,9 @@ const validate = (req, res, next) => {
 
 router.
 route('/')
-    .get((req, res) => {
-        res.status(200).json('전체 도서 조회');
-    })
-    .get((req, res) => {
-        const categoryId = req.query.categoryId;
-        const isNew = req.query.new;
+    .get(selectAll)
+    .get(selectCategory);
 
-        res.json('카테고리별 도서 목록 조회');
-    });
-
-
-router.get('/:booksId', (req, res) => {
-    const bookId = req.params.booksId;
-    
-    res.json('개별 도서 조회');
-});
+router.get('/:booksId', selectSingleBook);
 
 module.exports = router;
