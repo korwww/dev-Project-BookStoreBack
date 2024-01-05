29b14afa6b489e154f48e21533996ba7411feb7a
LeeUijin <n2n23339@gmail.com>
2024-01-05 22:32:42 +0900

    Implement : UserController.js login function

--- controller/UserContorller.js
+++ controller/UserContorller.js
@@ -1,4 +1,10 @@
 const conn = require('../mariadb');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const {StatusCodes} = require('http-status-codes');
+
+dotenv.config();
+router.use(express.json());
 
 const join = (req, res) => {
     const { email, password } = req.body;
@@ -18,8 +24,38 @@ const join = (req, res) => {
     );
 }
 
-const login = (req, res)=>{
-    res.json('로그인');
+const login = (req, res)=>(req, res) => {
+    const { email, password } = req.body;
+
+    let sql = `select * from users where email = ?`;
+    conn.query(sql, email,
+        function (err, results) {
+            var loginUser = results[0];
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            if (loginUser && loginUser.password == password) {
+                const token = jwt.sign({
+                    email : loginUser.email
+                }, process.env.PRIVATE_KEY, {
+                    expiresIn : '30m',
+                    issuer : "me"
+                });
+
+                res.cookie("token", token, {
+                    httpOnly : true
+                });
+
+                res.status(StatusCodes.OK).json(results);
+            } else {
+                res.status(StatusCodes.UNAUTHORIZED).json({
+                    message: `이메일 또는 비밀번호가 틀렸습니다.`
+                });
+            }
+        }
+    );
 };
 
 const passwordResetRequest =  (req, res)=>{
