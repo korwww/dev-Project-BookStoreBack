61e12cb8e0d1fcbe28c843e0d1ba1f4181ea4602
LeeUijin <n2n23339@gmail.com>
2024-01-17 16:13:29 +0900

    BugFix : orderController.js order() apply async/await

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,51 +1,37 @@
 const conn = require('../mariadb');
+const mariadb = require('mysql2/promise');
 const { StatusCodes } = require('http-status-codes');
 
 const orderController = {
-    order: (req, res) => {
+    order: async (req, res) => {
+        const conn = await mariadb.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'BookShop',
+            dateStrings: true
+        });
+
         const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } = req.body;
-        let delivery_id;
-        let order_id;
+
         let sql = `INSERT INTO delivery (address, receiver, contact) VALUES(?, ?, ?);`;
         let values = [delivery.address, delivery.receiver, delivery.contact];
-        conn.query(sql, values, 
-            function (err, results) {
-                if (err) {
-                    console.log(err);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
-
-                delivery_id = results.insertId;
-            }
-        );
+        let [results] = await conn.execute(sql, values);
+        let delivery_id = results.insertId;
+        
         sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
                 VALUES(?, ?, ?, ?, ?);`
         values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-        conn.query(sql, values,
-            function (err, results) {
-                if (err) {
-                    console.log(err);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
+        [results] = await conn.execute(sql, values);
 
-                order_id = results.insertId;
-            }
-        );
         sql = `INSERT INTO orderedBook(order_id, book_id, quantity) VALUES ?;`
         values = [];
         items.forEach((item) =>
             values.push([order_id, item.book_id, item.quantity])
         );
-        conn.query(sql, [values],
-            function (err, results) {
-                if (err) {
-                    console.log(err);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
+        results = await conn.execute(sql, [values]);
 
-                return res.status(StatusCodes.CREATED).json(results);
-            }
-        );
+        return res.status(StatusCodes.OK).json(results[0]);
     },
     getOrders: (req, res) => {
         res.status(200).json('주문 목록 조회');
--- mariadb.js
+++ mariadb.js
@@ -9,3 +9,28 @@ const connection = mysql.createConnection({
 });
 
 module.exports = connection;
+
+// const mysql = require('mysql2/promise');
+
+// const db_info = {
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'BookShop',
+//   multipleStatements: true,
+//   dateStrings: true,
+//   enableKeepAlive: true
+// }
+
+// module.exports = {
+//   init: () => {
+//       return mysql.createPool(db_info);
+//   },
+//   connect: (conn) => {
+//       conn.connect(err => {
+//           if (err) console.error('mysql connection error : ' + err);
+//           else console.log('mysql is connected successfully!');
+//       });
+//   },
+//   db_info
+// }
