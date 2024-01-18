ee60cdf41839b9b2c38d40969c4ddabc8dc9cfde
LeeUijin <n2n23339@gmail.com>
2024-01-19 05:40:39 +0900

    Implement : order() add sql delete cartItems

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -2,6 +2,13 @@ const conn = require('../mariadb');
 const mariadb = require('mysql2/promise');
 const { StatusCodes } = require('http-status-codes');
 
+const deleteCartItems = async (conn, items) => {
+    let sql = `DELETE FROM cartItems WHERE id IN (?);`;
+
+    let [result] = await conn.query(sql, [items]);
+    return result;
+}
+
 const orderController = {
     order: async (req, res) => {
         const conn = await mariadb.createConnection({
@@ -12,26 +19,35 @@ const orderController = {
             dateStrings: true
         });
 
-        const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } = req.body;
+        const { items, delivery, firstBookTitle, totalQuantity, totalPrice, userId } = req.body;
 
         let sql = `INSERT INTO delivery (address, receiver, contact) VALUES(?, ?, ?);`;
         let values = [delivery.address, delivery.receiver, delivery.contact];
         let [results] = await conn.execute(sql, values);
-        let delivery_id = results.insertId;
-        
+        let order_id = results.insertId;
+
         sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
                 VALUES(?, ?, ?, ?, ?);`
-        values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+        values = [firstBookTitle, totalQuantity, totalPrice, userId, order_id];
         [results] = await conn.execute(sql, values);
 
-        sql = `INSERT INTO orderedBook(order_id, book_id, quantity) VALUES ?;`
+        sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`;
+        let [orderItems, fileds] = await conn.query(sql, [items]);
+
+        sql = `INSERT INTO orderedBook(order_id, book_id, quantity) VALUES ?;`;
         values = [];
-        items.forEach((item) =>
+        orderItems.forEach((item) =>
             values.push([order_id, item.book_id, item.quantity])
         );
-        results = await conn.execute(sql, [values]);
 
-        return res.status(StatusCodes.OK).json(results[0]);
+        try{
+            results = await conn.query(sql, [values]);
+        }catch(err){
+            console.log("삽입 실패! cartItem이 없어 null값이 들어가므로 not null 제약에 의해 에러");
+        }
+
+        let result = await deleteCartItems(conn, items);
+        return res.status(StatusCodes.OK).json(result);
     },
     getOrders: (req, res) => {
         res.status(200).json('주문 목록 조회');
