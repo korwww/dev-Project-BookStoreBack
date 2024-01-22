9f509544f7de5c31b3b1efff96c6621115f1ec57
LeeUijin <n2n23339@gmail.com>
2024-01-22 22:56:35 +0900

    Refactor : User

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,23 +1,10 @@
-const conn = require('../database/mariadb');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const crypto = require('crypto');
 const {StatusCodes} = require('http-status-codes');
-
-dotenv.config();
+const AuthService = require('../services/UserService');
 
 const join = async (req, res) => {
-    const { email, password } = req.body;
-
-    const salt = crypto.randomBytes(10).toString('base64');
-    const hashPassword = crypto.pbkdf2Sync(password, salt, 10000, 10, 'sha512').toString('base64');
-
-    let sql = `INSERT INTO users (email, password, salt) VALUES(?, ?, ?)`;
-    let values = [email, hashPassword, salt];
-
     try {
-        const results = await conn.query(sql, values);
-        if(results.affectedRows) return res.status(StatusCodes.CREATED).json(results);
+        const user = await AuthService.join(req.body.email, req.body.password);
+        if(user) return res.status(StatusCodes.CREATED).json(user);
         return res.status(StatusCodes.BAD_REQUEST).end();
     } catch (err) {
         console.log(err);
@@ -26,31 +13,13 @@ const join = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-
-    let sql = `select * from users where email = ?`;
     try {
-        const [results] = await conn.query(sql, email);
-        const loginUser = results[0];
-
-        const hashPassword = crypto.pbkdf2Sync(password, loginUser.salt, 10000, 10, 'sha512').toString('base64');
-
-        if (loginUser && loginUser.password == hashPassword) {
-            const token = jwt.sign({
-                id : loginUser.id,
-                email : loginUser.email
-            }, process.env.PRIVATE_KEY, {
-                expiresIn : '30m',
-                issuer : "me"
-            });
-
-            res.cookie("token", token, {
-                httpOnly : true
-            });
-
-            res.status(StatusCodes.OK).json(results);
+        const token = await AuthService.login(req.body.email, req.body.password);
+        if (token) {
+            res.cookie("token", token, { httpOnly : true });
+            return res.status(StatusCodes.OK).json({token});
         } else {
-            res.status(StatusCodes.UNAUTHORIZED).json({
+            return res.status(StatusCodes.UNAUTHORIZED).json({
                 message: `이메일 또는 비밀번호가 틀렸습니다.`
             });
         }
@@ -60,17 +29,11 @@ const login = async (req, res) => {
     }
 };
 
-const passwordResetRequest = async (req, res)=>{
-    const { email } = req.body;
-
-    let sql = `select * from users where email = ?`;
+const passwordResetRequest = async (req, res) => {
     try {
-        const [results] = await conn.query(sql, email);
-        const user = results[0];
-        if (user) {
-            return res.status(StatusCodes.OK).json({
-                email : email
-            });
+        const email = await AuthService.passwordResetRequest(req.body.email);
+        if (email) {
+            return res.status(StatusCodes.OK).json({email});
         } else {
             return res.status(StatusCodes.UNAUTHORIZED).end();
         }
@@ -81,28 +44,19 @@ const passwordResetRequest = async (req, res)=>{
 };
 
 const passwordReset = async (req, res) => {
-    const {email, password} = req.body;
-
-    sql = `UPDATE users SET password=?, salt=? WHERE email=?`;
-
-    const salt = crypto.randomBytes(10).toString('base64');
-    const hashPassword = crypto.pbkdf2Sync(password, salt, 10000, 10, 'sha512').toString('base64');
-
-    let values = [hashPassword, salt, email];
     try {
-        const results = await conn.query(sql, values);
-        if(results.affectedRows == 0) return res.status(StatusCodes.BAD_REQUEST).end();
-        else return res.status(StatusCodes.OK).json(results);
+        const user = await AuthService.passwordReset(req.body.email, req.body.password);
+        if(user) return res.status(StatusCodes.OK).json(user);
+        return res.status(StatusCodes.BAD_REQUEST).end();
     } catch (err) {
         console.log(err);
         return res.status(StatusCodes.BAD_REQUEST).end();
     }
 };
 
-
-module.exports = Object.freeze({
+module.exports = {
     join,
     login,
     passwordResetRequest,
     passwordReset
-});
+};
---
+++ src/models/UserModel.js
@@ -0,0 +1,35 @@
+const crypto = require('crypto');
+const conn = require('../database/mariadb');
+
+class User {
+    constructor(email, password, salt) {
+        this.email = email;
+        this.password = password;
+        this.salt = salt || crypto.randomBytes(10).toString('base64');
+    }
+
+    static async findByEmail(email) {
+        const sql = `SELECT * FROM users WHERE email = ?`;
+        const [results] = await conn.query(sql, email);
+        return results[0] ? new User(results[0].email, results[0].password, results[0].salt) : null;
+    }
+
+    async save() {
+        const sql = `INSERT INTO users (email, password, salt) VALUES(?, ?, ?)`;
+        const values = [this.email, this.password, this.salt];
+        return await conn.query(sql, values);
+    }
+
+    async updatePassword() {
+        const sql = `UPDATE users SET password=?, salt=? WHERE email=?`;
+        const values = [this.password, this.salt, this.email];
+        return await conn.query(sql, values);
+    }
+
+    verifyPassword(password) {
+        const hashPassword = crypto.pbkdf2Sync(password, this.salt, 10000, 10, 'sha512').toString('base64');
+        return this.password === hashPassword;
+    }
+}
+
+module.exports = User;
