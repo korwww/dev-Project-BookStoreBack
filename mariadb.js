5a32b00f4471c74819603d155144dc3fee7f77f0
LeeUijin <n2n23339@gmail.com>
2024-01-22 09:39:18 +0900

    database values adapt .env

--- mariadb.js
+++ mariadb.js
@@ -1,36 +1,13 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'BookShop',
-  dateStrings: true
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  dateStrings: process.env.dateStrings
 });
 
 module.exports = connection;
-
-// const mysql = require('mysql2/promise');
-
-// const db_info = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'root',
-//   database: 'BookShop',
-//   multipleStatements: true,
-//   dateStrings: true,
-//   enableKeepAlive: true
-// }
-
-// module.exports = {
-//   init: () => {
-//       return mysql.createPool(db_info);
-//   },
-//   connect: (conn) => {
-//       conn.connect(err => {
-//           if (err) console.error('mysql connection error : ' + err);
-//           else console.log('mysql is connected successfully!');
-//       });
-//   },
-//   db_info
-// }
