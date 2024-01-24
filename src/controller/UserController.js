0711f5131d8030e9dfccc1f9b828c1c6f80a721a
LeeUijin <n2n23339@gmail.com>
2024-01-24 21:32:31 +0900

    Refactor : UserController.js remove else

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,10 +1,10 @@
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const UserService = require('../services/UserService');
 
 const join = async (req, res) => {
     try {
         const user = await UserService.join(req.body.email, req.body.password);
-        if(user) return res.status(StatusCodes.CREATED).json(user);
+        if (user) return res.status(StatusCodes.CREATED).json(user);
         return res.status(StatusCodes.BAD_REQUEST).end();
     } catch (err) {
         console.log(err);
@@ -16,13 +16,13 @@ const login = async (req, res) => {
     try {
         const token = await UserService.login(req.body.email, req.body.password);
         if (token) {
-            res.cookie("token", token, { httpOnly : true });
-            return res.status(StatusCodes.OK).json({token});
-        } else {
-            return res.status(StatusCodes.UNAUTHORIZED).json({
-                message: `이메일 또는 비밀번호가 틀렸습니다.`
-            });
+            res.cookie("token", token, { httpOnly: true });
+            return res.status(StatusCodes.OK).json({ token });
         }
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            message: `이메일 또는 비밀번호가 틀렸습니다.`
+        });
+
     } catch (err) {
         console.log(err);
         return res.status(StatusCodes.BAD_REQUEST).end();
@@ -33,10 +33,9 @@ const passwordResetRequest = async (req, res) => {
     try {
         const email = await UserService.passwordResetRequest(req.body.email);
         if (email) {
-            return res.status(StatusCodes.OK).json({email});
-        } else {
-            return res.status(StatusCodes.UNAUTHORIZED).end();
+            return res.status(StatusCodes.OK).json({ email });
         }
+        return res.status(StatusCodes.UNAUTHORIZED).end();
     } catch (err) {
         console.log(err);
         return res.status(StatusCodes.BAD_REQUEST).end();
@@ -46,7 +45,7 @@ const passwordResetRequest = async (req, res) => {
 const passwordReset = async (req, res) => {
     try {
         const user = await UserService.passwordReset(req.body.email, req.body.password);
-        if(user) return res.status(StatusCodes.OK).json(user);
+        if (user) return res.status(StatusCodes.OK).json(user);
         return res.status(StatusCodes.BAD_REQUEST).end();
     } catch (err) {
         console.log(err);
