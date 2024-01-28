f8709927afabc12359135e9dc87c22966f3791d1
LeeUijin <n2n23339@gmail.com>
2024-01-29 08:20:21 +0900

    Refactor : Services format synchronization

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,20 +1,17 @@
 const Order = require('../models/OrderModel');
 
-class OrderService {
-    static async createOrder(userId, items, delivery, firstBookTitle, totalQuantity, totalPrice) {
+const OrderService = {
+    async createOrder(userId, items, delivery, firstBookTitle, totalQuantity, totalPrice) {
         const order = new Order(userId, items, delivery, firstBookTitle, totalQuantity, totalPrice);
         const orderId = await order.save();
         const result = await order.deleteCartItems();
         return {orderId, result};
-    }
-
-    static async getOrders(userId) {
+    },
+    async getOrders(userId) {
         return await Order.findByUserId(userId);
-    }
-
-    static async getOrderDetail(orderId) {
+    },
+    async getOrderDetail(orderId) {
         return await Order.findById(orderId);
     }
 }
-
-module.exports = OrderService;
+module.exports = Object.freeze(OrderService);
